2071b8360d389b490c5dc3f50842230bcc61dff2
Jochen Reißinger <happyfour972@gmail.com>
2020-11-15 09:32:36 +0100

    add read file functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,33 @@ import { CookbookButton } from './components/CookbookButton/cookbookButton';
 
 export class App extends React.Component<{}> {
 	
+	uploadFile: () => void = () => {
+		var x = document.getElementById("fileUpload");
+		if(x){
+			x.click();
+		}
+	}
+	
+	readJsonFile: () => void = () => {
+		var x = document.getElementById("fileUpload") as HTMLInputElement;
+		if(x && x.files && x.files.length === 1){
+			var importedFile = x.files[0];
+			var reader = new FileReader();
+			reader.onload = function() {
+				console.log(reader.result);
+			};
+			reader.readAsText(importedFile);
+		}
+	}
+	
 	render() {
 		return (
 			<div className="App">
 				<TabBar/>
-				<div style={{'display': 'flex'}}>
+				<div>
+					<input type="file" id="fileUpload" style={{'display': 'none'}} onChange={() => this.readJsonFile()}/>
 					<CookbookButton label={"Filter"}/>
-					<CookbookButton label={"Import"}/>
+					<CookbookButton label={"Import"} onClick={() => this.uploadFile()}/>
 					<CookbookButton label={"Export"}/>
 				</div>
 			</div>
--- src/components/CookbookButton/cookbookButton.tsx
+++ src/components/CookbookButton/cookbookButton.tsx
@@ -5,14 +5,21 @@ import { GlobalState } from '../../redux/initialState';
 import { Dispatch } from 'redux';
 
 interface CookbookButtonProps {
-	label?: string
+	label?: string,
+	onClick?: () => void
 }
 
 class UnconnectedCookbookButton extends React.Component<CookbookButtonProps> {
+	
+	handleClick: () => void = () => {
+		if(this.props.onClick){
+			this.props.onClick();
+		}
+	}
 		
 	render(){			
 		return (
-			<div className={'CookbookButton'}>
+			<div className={'CookbookButton'} onClick={() => this.handleClick()}>
 				{this.props.label}
 			</div>
 		);

30bb3896e269a65d0fe4148f1074503bcd788345
Jochen Reißinger <happyfour972@gmail.com>
2020-11-18 22:31:14 +0100

    handle json parsing exception

--- src/components/LoadingModePage/loadingModePage.tsx
+++ src/components/LoadingModePage/loadingModePage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './loadingModePage.css';
-import { GlobalState, LOADING_VIEW } from '../../redux/initialState';
-import { getView } from '../../redux/selectors';
+import { GlobalState, LOADING_VIEW, Cookbook } from '../../redux/initialState';
+import { getView, getCookbookString } from '../../redux/selectors';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { setCookbook } from '../../redux/action_creators/BookState';
 
 interface LoadingModePageProps {
-	view: string
+	view: string,
+	getCookbookString: () => string,
+	setCookbook: (cookbook: Cookbook) => void
 }
 
 class UnconnectedLoadingModePage extends React.Component<LoadingModePageProps> {
@@ -18,19 +21,39 @@ class UnconnectedLoadingModePage extends React.Component<LoadingModePageProps> {
 		return 'LoadingModePage--hide';
 	}
 	
+	isJson = (input: string) => {
+		try {
+			const maybeJson = JSON.parse(input);
+		} catch(e) {
+			return false;
+		}
+		return true;
+	}
+	
 	render() {
+		const input = this.props.getCookbookString();
+		if(this.props.view === LOADING_VIEW && input !== "") {
+			if(this.isJson(input)) {
+				console.log("JSON!");
+			} else {
+				console.log("NOT A JSON!")
+			}
+		}
 		return (
 			<div className={this.getRootClassName()}>
-				In loading mode
+				loading...
 			</div>
 		);
 	}
 }
 
 const mapStateToProps = (state: GlobalState) => ({
-	view: getView(state)
+	view: getView(state),
+	getCookbookString: () => getCookbookString(state)
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+	setCookbook: (cookbook: Cookbook) => dispatch(setCookbook(cookbook))
+});
 
 export const LoadingModePage = connect(mapStateToProps, mapDispatchToProps)(UnconnectedLoadingModePage);
--- src/redux/selectors.tsx
+++ src/redux/selectors.tsx
@@ -8,3 +8,6 @@ export const getPassword: (state: GlobalState) => string =
 	
 export const getCookbook: (state: GlobalState) => Cookbook | null =
 	(state) => state.bookState.cookbook || null;
+	
+export const getCookbookString: (state: GlobalState) => string =
+	(state) => state.bookState.cookbookString || "";

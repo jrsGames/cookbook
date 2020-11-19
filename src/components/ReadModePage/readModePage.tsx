a6d556c0d4994d20f62f886987036b04a9c98195
Jochen Reißinger <happyfour972@gmail.com>
2020-11-19 22:32:12 +0100

    style header in readmodepage

--- src/components/ReadModePage/readModePage.tsx
+++ src/components/ReadModePage/readModePage.tsx
@@ -5,6 +5,8 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { getCookbook } from '../../redux/selectors';
 import { EMPTY_COOKBOOK } from '../UploadInput/uploadInput';
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 interface ReadModePageProps {
 	getCookbook: () => Cookbook
@@ -16,11 +18,18 @@ class UnconnectedReadModePage extends React.Component<ReadModePageProps> {
 		const cookbook: Cookbook = this.props.getCookbook();
 		return (
 			<div className="ReadModePage">
-				<div className="TitleBar">
-					{cookbook.title}
-				</div>
-				{cookbook.recipes.map((recipe) => {
-					return <div> {recipe.name} </div>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton edge="start" color="inherit" aria-label="menu">
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6">
+							{cookbook.title}
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				{cookbook.recipes.map((recipe, index) => {
+					return <div key={index}> {recipe.name} </div>
 				})}
 				<div className="CookbookContent">
 					{}

ceb782c585f73217611b03ee4b8bc713563794a1
Jochen Reißinger <happyfour972@gmail.com>
2020-11-19 11:06:19 +0100

    define already in App which page to show

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import { UploadInput } from './components/UploadInput/uploadInput';
 import { EntryPage } from './components/EntryPage/entryPage';
 import { CreateModePage } from './components/CreateModePage/createModePage';
 import { ReadModePage } from './components/ReadModePage/readModePage';
-import { LoadingModePage } from './components/LoadingModePage/loadingModePage';
+import { getView } from './redux/selectors';
+import { GlobalState, ENTRY_VIEW, CREATE_VIEW, READ_VIEW } from './redux/initialState';
+import { connect } from 'react-redux';
 
-export class App extends React.Component<{}> {
+interface AppProps {
+	getView: () => string
+}
+
+class UnconnectedApp extends React.Component<AppProps> {
 	
 	uploadFile: () => void = () => {
 		var x = document.getElementById("fileUpload");
@@ -16,14 +22,24 @@ export class App extends React.Component<{}> {
 	}
 
 	render() {
+		let content = <div />;
+		switch(this.props.getView()) {
+			case ENTRY_VIEW: {content = <EntryPage/>; break;}
+			case CREATE_VIEW: {content = <CreateModePage/>; break;}
+			case READ_VIEW: {content = <ReadModePage/>; break;}
+			default: break;
+		}
 		return (
 			<div className="App">
 				<UploadInput/>
-				<EntryPage/>
-				<LoadingModePage/>
-				<CreateModePage/>
-				<ReadModePage/>
+				{content}
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state: GlobalState) => ({
+	getView: () => getView(state)
+});
+
+export const App = connect(mapStateToProps)(UnconnectedApp);
